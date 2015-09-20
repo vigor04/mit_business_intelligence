db51047791130395fc9381e8e367190e6f67ddd8
Hideshi Ogoshi <hideshi.ogoshi@gmail.com>
2015-09-20 12:15:45 +0800

    Created DbConnection class

---
+++ lib/db.php
@@ -0,0 +1,27 @@
+<?php
+class DbConnection {
+    $dsn = 'mysql:dbname=;host=localhost';
+    $user = 'root';
+    $password = '';
+    $conn = NULL;
+    function __construct() {
+        $this->conn = new PDO($dsn, $user, $password);
+    }
+    function execute($sql) {
+        try {
+            $stmt = $dbh->prepare($sql);
+            $stmt->execute();
+            $result = [];
+            foreach ($stmt->fetch() as $row) {
+                $result[] = $row;
+            }
+            return $result;
+        } catch (PDOException $e) {
+            print('Error:'.$e->getMessage());
+            die();
+        } finally {
+            $stmt->closeCursor();
+            $this->conn = null;
+        }
+    }
+}
