d99d328738ef434bdb94063e401cad541da63c49
Hideshi Ogoshi <hideshi.ogoshi@gmail.com>
2015-09-26 20:55:11 +0800

    Renamed name of gender table to genders

--- config/create_table.sql
+++ config/create_table.sql
@@ -55,7 +55,7 @@ CREATE TABLE `martial_statuses` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `gender` (
+CREATE TABLE `genders` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(10) NOT NULL,
   PRIMARY KEY (`id`)
